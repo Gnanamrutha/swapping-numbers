66713622af891e8fc4a369749a236348ddf9ee4a
Gnanamrutha <92074108+Gnanamrutha@users.noreply.github.com>
2021-10-07 11:19:02 +0530

    Create swapping numbers.c

---
+++ swapping numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+int a,b;
+printf("Enter first number\n");
+scanf("%d",&a);
+printf("Enter second number\n");
+scanf("%d",&b);
+printf("the numbers before swapping are:%d,%d\n",a,b);
+printf(the numbers after swapping are:%d,%d\n",b,a);
+}
